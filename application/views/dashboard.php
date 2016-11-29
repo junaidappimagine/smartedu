92702c9a43df8e4f1d6f36362bdb62db82b7d6de
rafeeq-appnlogic <rafeeq@appnlogic.com>
2016-11-29 21:27:58 +0530

    dashboard file added

---
+++ application/views/dashboard.php
@@ -0,0 +1,139 @@
+<!-- begin #content -->
+		<div id="content" class="content">
+			<!-- begin breadcrumb -->
+			<ol class="breadcrumb pull-right">
+				<li><a href="javascript:;">Home</a></li>
+				<li class="active">Dashboard</li>
+			</ol>
+			<!-- end breadcrumb -->
+			<!-- begin page-header -->
+			<h1 class="page-header">Dashboard <small></small></h1>
+			<!-- end page-header -->
+			
+			<!-- begin row -->
+			<div class="row">
+				<!-- begin col-3 -->
+				<div class="col-md-3 col-sm-6">
+					<div class="widget widget-stats bg-green">
+						<div class="stats-icon"><i class="fa fa-desktop"></i></div>
+						<div class="stats-info">
+							<h4>TOTAL STUDENTS</h4>
+							<p>3,291,922</p>	
+						</div>
+						<div class="stats-link">
+							<a href="javascript:;">View Detail <i class="fa fa-arrow-circle-o-right"></i></a>
+						</div>
+					</div>
+				</div>
+				<!-- end col-3 -->
+				<!-- begin col-3 -->
+				<div class="col-md-3 col-sm-6">
+					<div class="widget widget-stats bg-blue">
+						<div class="stats-icon"><i class="fa fa-chain-broken"></i></div>
+						<div class="stats-info">
+							<h4>FEES DUE</h4>
+							<p>20.44%</p>	
+						</div>
+						<div class="stats-link">
+							<a href="javascript:;">View Detail <i class="fa fa-arrow-circle-o-right"></i></a>
+						</div>
+					</div>
+				</div>
+				<!-- end col-3 -->
+				<!-- begin col-3 -->
+				<div class="col-md-3 col-sm-6">
+					<div class="widget widget-stats bg-purple">
+						<div class="stats-icon"><i class="fa fa-users"></i></div>
+						<div class="stats-info">
+							<h4>STAFFS ON LEAVE</h4>
+							<p>15</p>	
+						</div>
+						<div class="stats-link">
+							<a href="javascript:;">View Detail <i class="fa fa-arrow-circle-o-right"></i></a>
+						</div>
+					</div>
+				</div>
+				<!-- end col-3 -->
+				<!-- begin col-3 -->
+				<!-- <div class="col-md-3 col-sm-6">
+					<div class="widget widget-stats bg-red">
+						<div class="stats-icon"><i class="fa fa-clock-o"></i></div>
+						<div class="stats-info">
+							<h4>AVG TIME ON SITE</h4>
+							<p>00:12:23</p>	
+						</div>
+						<div class="stats-link">
+							<a href="javascript:;">View Detail <i class="fa fa-arrow-circle-o-right"></i></a>
+						</div>
+					</div>
+				</div> -->
+				<!-- end col-3 -->
+			</div>
+			<!-- end row -->
+
+			<!-- begin col-8 -->
+			    <div class="col-md-8">
+			        <!-- begin panel -->
+			        <div class="panel panel-inverse" data-sortable-id="index-4">
+			            <div class="panel-heading">
+			                <h4 class="panel-title">Quick Links </h4>
+			            </div>
+			            <div class="panel-body">
+                        <ul class="registered-users-list clearfix">
+                            <li>
+                                <a href="javascript:;"><img src="<?php echo base_url(); ?>assets/img/admission.png" alt="" /></a>
+                                <!-- <h4 class="username text-ellipsis">
+                                    Student Admission
+                                    <!-- <small>Algerian</small> 
+                                </h4> -->
+                            </li>
+                            <li>
+                                <a href="javascript:;"><img  src="<?php echo base_url(); ?>assets/img/Events-Icon.png" alt="" /></a>
+                               <!--  <h4 class="username text-ellipsis">
+                                    Event Creation
+                                    <!-- <small>Korean</small> 
+                                </h4> -->
+                            </li>
+                            <li>
+                                <a href="javascript:;"><img src="<?php echo base_url(); ?>assets/img/news_images.png" alt="" /></a>
+                                <!-- <h4 class="username text-ellipsis">
+                                    News
+                                    <!-- <small>Indian</small> 
+                                </h4> -->
+                            </li>
+                            <li>
+                                <a href="javascript:;"><img src="<?php echo base_url(); ?>assets/img/employee_icon.jpg" alt="" /></a>
+                               <!--  <h4 class="username text-ellipsis">
+                                    Employee Search 
+                                    <!-- <small>Japanese</small> 
+                                </h4> -->
+                            </li>
+                            <li>
+                                <a href="javascript:;"><img src="<?php echo base_url(); ?>assets/img/hr.png" alt="" /></a>
+                               <!--  <h4 class="username text-ellipsis">
+                                    Human Rresource
+                                    <!-- <small>Cuban</small> --
+                                </h4> -->
+                            </li>
+                            <li>
+                                <a href="javascript:;"><img src="<?php echo base_url(); ?>assets/img/payroll.png" alt="" /></a>
+                                <!-- <h4 class="username text-ellipsis">
+                                    PayRoll and PaySlip
+                                    <!-- <small>Nepalese</small> 
+                                </h4> -->
+                            </li>
+                            
+                        </ul>
+                        </div>
+			            
+			        </div>
+			        <!-- end panel -->
+			    </div>
+			    <!-- end col-8 -->
+			    <!-- begin col-4 -->
+			    <div class="col-md-4">
+			        <!-- begin panel -->
+			        <!-- end panel -->
+			    </div>
+			    <!-- end col-4
+<!-- script load for this page -->
