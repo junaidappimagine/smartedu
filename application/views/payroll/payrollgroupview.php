8d6ae28bc16c75d008b9c430ebb8e2e39c5457ab
senthil-appnlogic <sentilkumar@appnlogic.com>
2016-11-29 19:54:46 +0530

    payroll group view page

---
+++ application/views/payroll/payrollgroupview.php
@@ -0,0 +1,16 @@
+
+
+<html>
+    <head>
+        
+    </head>
+    <body>
+         <form class="form-horizontal" action="<?php echo base_url(); ?>payrollgroup/Add_Payroll_Groupview/" method="post" enctype="multipart/form-data">
+            <div class="form-group">								
+                    <div class="col-md-offset-5">
+                            <button type="submit" class="btn btn-md btn-success">Create Payroll Groupview</button>
+                    </div>
+                </div>
+         </form>
+    </body>
+</html>
