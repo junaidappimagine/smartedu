6edcc89b1a56e873a97c66ec2d9046e2a4b4f70c
jaya-cloudlogic <jayasegar93@gmail.com>
2016-12-03 11:18:08 +0530

    employee_category.php

--- application/views/payroll/employee_category.php
+++ application/views/payroll/employee_category.php
@@ -30,36 +30,31 @@
                             <div class="well">
                                 <fieldset>
                                     <legend>Employee Category</legend>
-                                 <!-- <div class="panel-body">-->
-								     <div class="row">
-										<div class="col-md-4"><button type="button" class="btn btn-primary" id="Add" value="Add" data-toggle="modal" data-target="#myModal"><i class="fa fa-plus">Add</i></a></div>
+									<div class="row">
+										<div class="col-md-4"><button type="button" class="btn btn-primary btn-sm" id="Add" value="Add" data-toggle="modal" data-target="#myModal"><i class="fa fa-plus">Add</i></a></div>
 									 </div><br>
                                      <table class="table table-bordered table-responsive" >
                                          <thead>
-                                             <tr style="background-color: #d9edf7; border-color: #b6e2ef;">
-                                                 <th width="600px">Categories</th>
-                                                 <th width="300px">Action</th>
-                                                 
-                                            </tr>
+											<tr style="background-color: #d9edf7; border-color: #b6e2ef;">
+												<th width="600px">Categories</th>
+												<th width="300px">Action</th>
+											</tr>
                                         </thead>
                                          <tbody>
                                              <tr>
                                                  <td> <span class="badge badge-success">-</span> &nbsp;&nbsp;Category 1(C1)</td>
                                                  <td><button type="button"  name="edit" id="edit" value="edit" class="btn btn-xs btn-primary" data-toggle="modal" data-target="#myModal"><i class="fa fa-edit"></i></button>
                                                  <button type="button"  name="delete" class="btn btn-xs btn-danger"><i class="fa fa-trash-o"></i></button></td>
-                                                
                                             </tr>
                                             <tr>
                                                  <td><span class="badge badge-danger">-</span>&nbsp;&nbsp;Category 2(C2)</td>
                                                  <td><button type="button" name="edit"  id="edit2" value="edit2" class="btn btn-xs btn-primary" data-toggle="modal" data-target="#myModal"><i class="fa fa-edit"></i></button>
                                                  <button onclick="" name="delete" class="btn btn-xs btn-danger"><i class="fa fa-trash-o"></i></button></td>
-                                                
                                             </tr>
                                         </tbody>
                                      </table>
-                                   <!-- </div>-->
-                            </fieldset>
-                        </div>
+                                </fieldset>
+							</div>
                         <!--end well-->
                     </div>
                     <!-- end panel -->
@@ -113,101 +108,90 @@
         
         
 <!-- Modal -->
-  <div class="modal fade" id="myModal" role="dialog">
-    <div class="modal-dialog modal-md">
-    
-      <!-- Modal content-->
-      <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4 class="modal-title" id="header"></h4>
-        </div>
-        <div class="modal-body">
-           <!-- <div class="panel-group">
-                <div class="panel panel-primary">
-                   <div class="panel-heading">Edit Employee category</div>-->
-                       <div class="panel-body">
-						<div class="col-md-offset-3">
-                           <form method="" action="" class="form-horizontal">
-                               <div class="form-group">
-                                   <label class="col-md-2 control-label">Name</label>
-                                   <div class="col-md-4">
-                                       <input type="text" class="form-control input-sm" id="name" placeholder="" />
-                                   </div>
-                               </div>
-                               <div class="form-group">
-                                   <label class="col-md-2 control-label">Prefix</label>
-                                   <div class="col-md-4">
-                                       <input type="text" class="form-control input-sm" id="prefix" placeholder="" d/>
-                                   </div>
-                               </div>
-                               <div class="form-group">
-                                   <label class="col-md-2 control-label">Status</label>
-                                   <div class="col-md-6">
-									 <label class="radio-inline">
-										<input type="radio" name="optradio" id="radio_1">Active
-									  </label>
-									  <label class="radio-inline">
-										<input type="radio" name="optradio" id="radio_2">Inactive
-									</label>
-                                   </div>
-                               </div><br>
-                               <div class="form-group">
-                                       <label class="col-md-2 control-label"></label>
-                                       <div class="col-md-5">
-                                           <button type="button" class="btn btn-primary btn-sm" id="action" >Create</button>
-                                       </div>
-                               </div>
-                           </form>
+<div class="modal fade" id="myModal" role="dialog">
+	<div class="modal-dialog modal-md">
+		<!-- Modal content-->
+		<div class="modal-content">
+			<div class="modal-header">
+			  <button type="button" class="close" data-dismiss="modal">&times;</button>
+			  <h4 class="modal-title" id="header"></h4>
+			</div>
+			<div class="modal-body">
+            <div class="panel-body">
+			    <div class="col-md-offset-3">
+					<form method="" action="" class="form-horizontal">
+						<div class="form-group">
+							<label class="col-md-2 control-label">Name</label>
+							<div class="col-md-4">
+								<input type="text" class="form-control input-sm" id="name" placeholder="" />
+							</div>
 						</div>
-                       </div>
-                <!--</div>
-            </div>-->
-        </div>
-        <!--<div class="modal-footer">
-          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        </div>
-      </div>-->
-      
-    </div>
-  </div>
+						<div class="form-group">
+							<label class="col-md-2 control-label">Prefix</label>
+							<div class="col-md-4">
+								<input type="text" class="form-control input-sm" id="prefix" placeholder="" d/>
+							</div>
+						</div>
+						<div class="form-group">
+							<label class="col-md-2 control-label">Status</label>
+							<div class="col-md-6">
+							  <label class="radio-inline">
+								 <input type="radio" name="optradio" id="radio_1">Active
+							   </label>
+							   <label class="radio-inline">
+								 <input type="radio" name="optradio" id="radio_2">Inactive
+							 </label>
+							</div>
+						</div><br>
+						<div class="form-group">
+								<label class="col-md-2 control-label"></label>
+								<div class="col-md-5">
+									<button type="button" class="btn btn-primary btn-sm" id="action" >Create</button>
+								</div>
+						</div>
+					</form>
+				</div>
+		   </div>
+			</div>
+		</div>
+	</div>
+</div>
 <script>
-	
-	$(document).ready(function() {
-		$('button').click(function(){
-			console.log($(this).val());
-			if($(this).val()=='Add')
-			{
-				//alert();
-				$('.modal-title').text('Create Employee Category');
-				$('#action').text('Create');
-				$("#radio_1").prop("checked", false);
-				 $("#radio_2").prop("checked", false);
-			
-				$('#name').val('');
-				$('#prefix').val('');
-			}
+$(document).ready(function() {
+	$('button').click(function(){
+		console.log($(this).val());
+		if($(this).val()=='Add')
+		{
+			//alert();
+			$('.modal-title').text('Create Employee Category');
+			$('#action').text('Create');
+			$("#radio_1").prop("checked", false);
+			 $("#radio_2").prop("checked", false);
+		
+			$('#name').val('');
+			$('#prefix').val('');
+		}
 		else if($(this).val()=='edit')
-			{
-				//alert();
-				$('.modal-title').text('Edit Employee Category');
-				$('#action').text('Update');
-			    $("#radio_1").prop("checked", true);
-				$('#name').val('Category 1');
-				$('#prefix').val('C1');
-			}
-			else if($(this).val()=='edit2')
-			{
-				//alert();
-				$('.modal-title').text('Edit Employee Category');
-				$('#action').text('Update');
-			    $("#radio_2").prop("checked", true);
-				$("#radio_1").prop("checked", false);
-				$('#name').val('Category 2');
-				$('#prefix').val('C2');
-			}
-		});
+		{
+			//alert();
+			$('.modal-title').text('Edit Employee Category');
+			$('#action').text('Update');
+			$("#radio_1").prop("checked", true);
+			$('#name').val('Category 1');
+			$('#prefix').val('C1');
+		}
+		else if($(this).val()=='edit2')
+		{
+			//alert();
+			$('.modal-title').text('Edit Employee Category');
+			$('#action').text('Update');
+			$("#radio_2").prop("checked", true);
+			$("#radio_1").prop("checked", false);
+			$('#name').val('Category 2');
+			$('#prefix').val('C2');
+		}
 	});
+});
 </script>	
 	
 	
